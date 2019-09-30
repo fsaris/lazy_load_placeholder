35272db8afbac64278a9373cf5e784e69eb60759
Frans Saris <franssaris@gmail.com>
2019-09-30 20:52:17 +0200

    [TASK] Adjust dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3/cms-core' => '*',
         ],
         'conflicts' => [],
         'suggests' => [],
